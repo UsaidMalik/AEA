a9f81b9281c1a9f781c58aaf54fde7f7df45d74b
Usaid Malik <umalik2003@gmail.com>
2025-09-13 19:19:13 -0700

    updates to config

--- processing-engine/DBWriter/DBWriter.py
+++ processing-engine/DBWriter/DBWriter.py
@@ -1,12 +1,30 @@
-# import mongodb
+import os
+import datetime
+from pymongo import MongoClient
 
 
-
-class DBWriter():
+class DBWriter:
     def __init__(self):
-        pass
-    
+        # Pull env vars
+        db_uri = os.getenv("DATABASE_URI", "mongodb")
+        db_port = int(os.getenv("DATABASE_PORT", 27017))
+        db_name = os.getenv("DATABASE_NAME", "aea_local")
 
-    # A thread will call this function and then add its database call signature (that is the collection name)
-    def write_entry(collection, ):
-        pass
+        # Create MongoDB client
+        self.client = MongoClient(host=db_uri, port=db_port)
+
+        # You can choose a default database name here
+        self.db = self.client[db_name]
+
+    def write_entry(self, collection, data):
+        """
+        Writes a document to the specified collection.
+        :param collection: str - name of the MongoDB collection
+        :param data: dict - the document to insert
+        """
+        # Optionally add a timestamp if not present
+        if "ts" not in data:
+            data["ts"] = datetime.datetime.utcnow()
+
+        result = self.db[collection].insert_one(data)
+        return result.inserted_id
